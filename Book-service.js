2ba7e6c49f2317fc92bc8d18054aacc0040411c4
Jordy Alexis <jordy2806@hotmail.com>
2026-03-05 10:19:36 +0100

    Add files via upload

--- Book-service.js
+++ Book-service.js
@@ -1,6 +1,9 @@
+//import { environment } from "./environment/environment.js"; // importo la costante environment da environment.js
+import { environment } from "./environment/environment.dev.js"; // importo la costante environment da environment.dev.js per utilizzare l'url di sviluppo durante lo sviluppo e l'url di produzione durante la produzione
+
 export class BookService{  // BookService è una classe che contiene i metodi per interagire con l'API dei libri
 
-    static url = "https://gutendex.com/books/";  
+    //static url = "https://gutendex.com/books/";  
 
     constructor(){}
 
@@ -8,19 +11,22 @@ export class BookService{  // BookService è una classe che contiene i metodi pe
         //const url = "https://gutendex.com/books/";
 
         //return fetch(url)
-        return fetch(BookService.url) // utilizzo la proprietà statica
+        //return fetch(BookService.url) // utilizzo la proprietà statica
+        return fetch(environment.apiUrl) // utilizzo la costante importata da environment.js
         .then(resp => resp.json())
         .then(result => result);
     }
 
     getBookById(id){  //metodo per prendere il dettaglio di un singolo libro
-        return fetch(BookService.url + id) // id è il parametro che passo alla funzione ed è l'id del libro
+       // return fetch(BookService.url + id) // id è il parametro che passo alla funzione ed è l'id del libro
+        return fetch(environment.apiUrl + id) // id è il parametro che passo alla funzione ed è l'id del libro
         .then(resp => resp.json()) //converto la risposta in json
         .then(result => result);  //ritorno il risultato 
     }
 
     searchBooks(query){  //*   
-        const searchUrl = BookService.url + '?search=' + encodeURIComponent(query); //creo l'url di ricerca
+        //const searchUrl = BookService.url + '?search=' + encodeURIComponent(query); //creo l'url di ricerca
+        const searchUrl = environment.apiUrl + '?search=' + encodeURIComponent(query); //creo l'url di ricerca
         return fetch(searchUrl)
         .then(resp => resp.json())
         .then(result => result);
--- README.md
+++ README.md
@@ -11,3 +11,28 @@ Lezione di mercoledì 7 gennaio:
  - Altra libreria JS: https://material.angular.dev/
 
  - Pagination: https://getbootstrap.com/docs/5.3/components/pagination/
+
+
+----------------------------------------------------------------------------------------------------------------
+
+Lezione di giovedì 8 gennaio:
+
+cartella environment:  
+- environment.js: contiene l'url dell'API dei libri, in questo modo se voglio cambiare l'url dell'API basta che cambio il valore della proprietà apiUrl e non devo cercare in tutto il codice
+
+//  Metto l'url dell'API in una costante per poterla modificare facilmente in futuro senza dover cercare in tutto il codice
+
+// export const environment = {
+//     apiUrl: "https://gutendex.com/books/"  // definisco una costante per l'url dell'API dei libri
+// }
+
+export const environment = {
+    apiUrl: "https://gutendex.com/books/prod/"  // definisco una costante per l'url dell'API dei libri in produzione
+}
+
+
+- environment.dev.js: contiene l'url dell'API dei libri in sviluppo, in questo modo posso utilizzare l'url di sviluppo durante lo sviluppo e l'url di produzione durante la produzione
+
+export const environment = {
+    apiUrl: "https://gutendex.com/books/"  // definisco una costante per l'url dell'API dei libri in sviluppo
+}
